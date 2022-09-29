341dc3dd8403d6277ade2d8e795cd6d44c3a1ce4
Akir Oussama <akir.oussama@gmail.com>
2022-09-29 13:08:43 +0200

    add remove todo using swipable view

--- src/components/tasks-list.tsx
+++ src/components/tasks-list.tsx
@@ -5,6 +5,11 @@ import {
   ScrollView,
 } from "react-native-gesture-handler";
 import TaskItem from "./task-item";
+import { makeStyledComponent } from "../utils/styled";
+
+const StyledView = makeStyledComponent(View);
+const StyledScrollView = makeStyledComponent(ScrollView);
+
 interface TaskItemData {
   id: string;
   subject: string;
@@ -13,27 +18,57 @@ interface TaskItemData {
 
 interface TaskListProps {
   data: Array<TaskItemData>;
+  editingItemId: string | null;
   onToggleItem: (item: TaskItemData) => void;
+  onChangeSubject: (item: TaskItemData, newSubject: string) => void;
+  onFinishEditing: (item: TaskItemData) => void;
   onPressLabel: (item: TaskItemData) => void;
+  onRemoveItem: (item: TaskItemData) => void;
 }
 
 interface TaskItemProps
   extends Pick<PanGestureHandlerProps, "simultaneousHandlers"> {
   data: TaskItemData;
+  isEditing: boolean;
   onToggleItem: (item: TaskItemData) => void;
+  onChangeSubject: (item: TaskItemData, newSubject: string) => void;
+  onFinishEditing: (item: TaskItemData) => void;
   onPressLabel: (item: TaskItemData) => void;
+  onRemove: (item: TaskItemData) => void;
 }
 
 export const AnimatedTaskItem = (props: TaskItemProps) => {
-  const { simultaneousHandlers, data, onToggleItem, onPressLabel } = props;
+  const {
+    simultaneousHandlers,
+    data,
+    isEditing,
+    onToggleItem,
+    onChangeSubject,
+    onFinishEditing,
+    onPressLabel,
+    onRemove,
+  } = props;
   const handleToggleCheckbox = useCallback(() => {
     onToggleItem(data);
   }, [data, onToggleItem]);
+  const handleChangeSubject = useCallback(
+    (subject: string) => {
+      onChangeSubject(data, subject);
+    },
+    [data, onChangeSubject]
+  );
+  const handleFinishEditing = useCallback(() => {
+    onFinishEditing(data);
+  }, [data, onFinishEditing]);
   const handlePressLabel = useCallback(() => {
     onPressLabel(data);
   }, [data, onPressLabel]);
+  const handleRemove = useCallback(() => {
+    onRemove(data);
+  }, [data, onRemove]);
   return (
-    <View
+    <StyledView
+      w="full"
       from={{
         opacity: 0,
         scale: 0.5,
@@ -54,30 +89,46 @@ export const AnimatedTaskItem = (props: TaskItemProps) => {
         simultaneousHandlers={simultaneousHandlers}
         subject={data.subject}
         isDone={data.done}
+        isEditing={isEditing}
         onToggleCheckbox={handleToggleCheckbox}
+        onChangeSubject={handleChangeSubject}
+        onFinishEditing={handleFinishEditing}
         onPressLabel={handlePressLabel}
+        onRemove={handleRemove}
       />
-    </View>
+    </StyledView>
   );
 };
 
 export default function TaskList(props: TaskListProps) {
-  const { data, onToggleItem, onPressLabel } = props;
+  const {
+    data,
+    editingItemId,
+    onToggleItem,
+    onChangeSubject,
+    onFinishEditing,
+    onPressLabel,
+    onRemoveItem,
+  } = props;
   const refScrollView = useRef(null);
 
   return (
-    <ScrollView ref={refScrollView}>
+    <StyledScrollView ref={refScrollView} w="full">
       <AnimatePresence>
         {data.map(item => (
           <AnimatedTaskItem
             key={item.id}
             data={item}
             simultaneousHandlers={refScrollView}
+            isEditing={item.id === editingItemId}
             onToggleItem={onToggleItem}
+            onChangeSubject={onChangeSubject}
+            onFinishEditing={onFinishEditing}
             onPressLabel={onPressLabel}
+            onRemove={onRemoveItem}
           />
         ))}
       </AnimatePresence>
-    </ScrollView>
+    </StyledScrollView>
   );
 }
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -32,6 +32,7 @@ const initialData = [
 
 export default function Main() {
   const [data, setData] = useState(initialData);
+  const [editingItemId, setEditingItemId] = useState<string | null>(null);
 
   const handleToggleTaskItem = useCallback(item => {
     setData(prevData => {
@@ -41,6 +42,29 @@ export default function Main() {
         ...item,
         done: !item.done,
       };
+      return newData;
+    });
+  }, []);
+  const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+    setData(prevData => {
+      const newData = [...prevData];
+      const index = prevData.indexOf(item);
+      newData[index] = {
+        ...item,
+        subject: newSubject,
+      };
+      return newData;
+    });
+  }, []);
+  const handleFinishEditingTaskItem = useCallback(_item => {
+    setEditingItemId(null);
+  }, []);
+  const handlePressTaskItemLabel = useCallback(item => {
+    setEditingItemId(item.id);
+  }, []);
+  const handleRemoveItem = useCallback(item => {
+    setData(prevData => {
+      const newData = prevData.filter(i => i !== item);
       return newData;
     });
   }, []);
@@ -56,7 +80,15 @@ export default function Main() {
         borderTopRightRadius="20px"
         pt="20px"
       >
-        <TaskList data={data} onToggleItem={handleToggleTaskItem} />
+        <TaskList
+          data={data}
+          onToggleItem={handleToggleTaskItem}
+          onChangeSubject={handleChangeTaskItemSubject}
+          onFinishEditing={handleFinishEditingTaskItem}
+          onPressLabel={handlePressTaskItemLabel}
+          onRemoveItem={handleRemoveItem}
+          editingItemId={editingItemId}
+        />
       </VStack>
     </Box>
   );
